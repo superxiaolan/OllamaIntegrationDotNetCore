306a33a8e3fd5ffc85b3a4a4d508ba164bfea683
SuperXiaoLan <923326728@qq.com>
2026-01-01 17:07:15 +0800

    feat:打通 ASP.NET Core -> Ollama -> qwen2.5-14b 这一整条链路

---
+++ src/OllamaIntegrationWebAPI/OllamaIntegrationWebAPI/Controllers/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OllamaSharp;
+
+namespace OllamaIntegrationWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IOllamaApiClient _ollamaApiClient;
+        public ChatController(IOllamaApiClient ollamaApiClient) 
+        { 
+            _ollamaApiClient = ollamaApiClient;
+        }
+
+        [HttpPost("api-stream")]
+        public async Task GetStream([FromBody] string prompt) 
+        {
+            Response.ContentType = "text/plain";
+
+            // 异步流式获取 Ollama 的响应
+            await foreach (var status in _ollamaApiClient.GenerateAsync(prompt))
+            {
+                if(status != null)
+                {
+                    // 实时写入 HTTP 响应体
+                    await Response.WriteAsync(status.Response);
+                    await Response.Body.FlushAsync();
+                }
+            }
+        }
+    }
+}
